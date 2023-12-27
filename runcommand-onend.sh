3bdd47dcaf2b065d06d3581cefee4ad66385dd80
snakepl88 <65735420+snakepl88@users.noreply.github.com>
2023-12-27 12:31:30 -0600

    Only using this for eduke32
    Only use this driver for eduke32
    Commented everything else out, test if needed

--- runcommand-onend.sh
+++ runcommand-onend.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-source /opt/retropie/configs/all/xboxdrvend.sh > /dev/null 2>&1
-sudo pkill -CONT mpg123 > /dev/null 2>&1
+# Only use this for eduke32, for now
+if [[ $2 == eduke32 ]]; then
+  echo "Executing xboxdrvend.sh for "$2 >> /dev/shm/runcommand.log
+  source /opt/retropie/configs/all/xboxdrvend.sh > /dev/null 2>&1
+fi
+
+# Not sure if needed
+#sudo pkill -CONT mpg123 > /dev/null 2>&1
+
+# Not sure if still needed for PSX
+#fbset -g 320 240 320 240 8
+#fbset -g 320 240 320 240 32
--- runcommand-onstart.sh
+++ runcommand-onstart.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-[[ $2 == openbor-6xxx ]] && filename=$(basename "$3") && cp "/opt/retropie/configs/ports/openbor/Saves/master2.cfg" "/opt/retropie/configs/ports/openbor/Saves/${filename%.*}.cfg"
-[[ $2 == openbor ]] && filename=$(basename "$3") && cp "/opt/retropie/configs/ports/openbor/Saves/master1.cfg" "/opt/retropie/configs/ports/openbor/Saves/${filename%.*}.cfg"
-source /opt/retropie/configs/all/xboxdrvstart.sh > /dev/null 2>&1
-sudo pkill -STOP mpg123 > /dev/null 2>&1
+# Only use this for eduke32, for now
+if [[ $2 == eduke32 ]]; then
+  echo "Executing xboxdrvstart.sh for "$2 >> /dev/shm/runcommand.log
+  source /opt/retropie/configs/all/xboxdrvstart.sh > /dev/null 2>&1
+fi
+
+# Not sure if needed
+#[[ $2 == openbor-6xxx ]] && filename=$(basename "$3") && cp "/opt/retropie/configs/ports/openbor/Saves/master2.cfg" "/opt/retropie/configs/ports/openbor/Saves/${filename%.*}.cfg"
+#[[ $2 == openbor ]] && filename=$(basename "$3") && cp "/opt/retropie/configs/ports/openbor/Saves/master1.cfg" "/opt/retropie/configs/ports/openbor/Saves/${filename%.*}.cfg"
+
+# Not sure if needed
+#sudo pkill -STOP mpg123 > /dev/null 2>&1
+
+# Not sure if still needed for PSX
+#fbset -g 320 240 640 480 16
