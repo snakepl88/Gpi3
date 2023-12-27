9f3ecd2039ad25302e74d3bf0de3111fc9cf4160
snakepl88 <65735420+snakepl88@users.noreply.github.com>
2023-12-27 11:04:04 -0600

    remove fbset
    Remove fbset, will test later if needed for PSX.

--- runcommand-onend.sh
+++ runcommand-onend.sh
@@ -2,5 +2,3 @@
 
 source /opt/retropie/configs/all/xboxdrvend.sh > /dev/null 2>&1
 sudo pkill -CONT mpg123 > /dev/null 2>&1
-fbset -g 320 240 320 240 8
-fbset -g 320 240 320 240 32
--- runcommand-onstart.sh
+++ runcommand-onstart.sh
@@ -4,4 +4,3 @@
 [[ $2 == openbor ]] && filename=$(basename "$3") && cp "/opt/retropie/configs/ports/openbor/Saves/master1.cfg" "/opt/retropie/configs/ports/openbor/Saves/${filename%.*}.cfg"
 source /opt/retropie/configs/all/xboxdrvstart.sh > /dev/null 2>&1
 sudo pkill -STOP mpg123 > /dev/null 2>&1
-fbset -g 320 240 640 480 16
